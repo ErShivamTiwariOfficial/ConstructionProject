beca64d2af82043ba7c20d82f19d2dec8b4aade1
shivamtiwari0511 <shiwamkumar61@gmail.com>
2025-02-15 20:54:40 +0530

    Problems Solve

--- src/main.jsx
+++ src/main.jsx
@@ -3,30 +3,30 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import i18next from "i18next";
-import HttpBackend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
-import { initReactI18next } from "react-i18next";
+// import i18next from "i18next";
+// import HttpBackend from "i18next-http-backend";
+// import LanguageDetector from "i18next-browser-languagedetector";
+// import { initReactI18next } from "react-i18next";
 
-const apiKey = "Q_TzgIhnc7ODiw6VndjsYA";
-const loadPath = `https://api.i18nexus.com/project_resources/translations/{{lng}}/{{ns}}.json?api_key=${apiKey}`;
+// const apiKey = "Q_TzgIhnc7ODiw6VndjsYA";
+// const loadPath = `https://api.i18nexus.com/project_resources/translations/{{lng}}/{{ns}}.json?api_key=${apiKey}`;
 
-i18next
-  .use(HttpBackend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: "en",
+// i18next
+//   .use(HttpBackend)
+//   .use(LanguageDetector)
+//   .use(initReactI18next)
+//   .init({
+//     fallbackLng: "en",
 
-    ns: ["default"],
-    defaultNS: "default",
+//     ns: ["default"],
+//     defaultNS: "default",
 
-    supportedLngs: ["en","mr","hi"],
+//     supportedLngs: ["en","mr","hi"],
     
-    backend: {
-      loadPath: loadPath
-    }
-  })
+//     backend: {
+//       loadPath: loadPath
+//     }
+//   })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
